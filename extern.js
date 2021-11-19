4f617da0531e668e4a2bcd49cb1e2757a5817a36
kapilrao2607 <94703101+kapilrao2607@users.noreply.github.com>
2021-11-19 22:13:46 +0530

    Add files via upload

---
+++ extern.js
@@ -0,0 +1,30 @@
+document.getElementById('box').style.height = "30px";
+document.getElementById('box').style.width = "300px";
+document.getElementById('btn').style.width = "80px";		
+document.getElementById('btn').style.height = "30px";
+
+function go(){
+	window.open("login.html");
+}
+
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
